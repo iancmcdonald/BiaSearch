b0bcf163524a2f96664a0d37b73ce7a81470b1b0
Tristan Burke <tristan_burke@berkeley.edu>
2017-07-26 20:20:08 -0700

    making purdy

--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -13,12 +13,19 @@ import {Article} from './article';
 export class SearchComponent {
   articles: Article[];
   url: string;
+  placeholder_toggle: boolean;
+  loading: boolean
 
   constructor(private searchService: SearchService) {
+    this.placeholder_toggle = true;
+    this.loading = false
   }
 
   getArticles(url: string): void {
+    this.placeholder_toggle = false;
+    this.loading = true;
     this.searchService.getArticles(url)
       .then(result => this.articles = result);
+    this.loading = false;
   }
 }
